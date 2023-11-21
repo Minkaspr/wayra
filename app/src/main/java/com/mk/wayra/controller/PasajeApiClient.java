d5d0d35a76e45dab20606ad9914e98538316d07e
Dario <106852390+Minkaspr@users.noreply.github.com>
2023-11-21 15:27:36 -0500

    comentarios

--- app/src/main/java/com/mk/wayra/controller/PasajeApiClient.java
+++ app/src/main/java/com/mk/wayra/controller/PasajeApiClient.java
@@ -4,6 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class PasajeApiClient {
+    // private static final String BASE_URL = "http://10.0.2.2/api_usuarios/";
     private static final String BASE_URL = "https://minka123.000webhostapp.com/";
     private static Retrofit retrofit;
 
